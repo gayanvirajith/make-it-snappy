6d11c740f66181aef6b64faafecb15ced633f613
Gayan Virajith <gayanvirajith@gmail.com>
2015-01-21 10:45:52 +0530

    Add users migration and validation rules

--- app/models/User.php
+++ app/models/User.php
@@ -9,6 +9,12 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 
 	use UserTrait, RemindableTrait;
 
+	public static $rules = array(
+		'username' => 'required|unique:users|alpha_dash|min:4',
+		'password' => 'required|alpha_num|between:4,8|confirmed',
+		'password_confirmation' => 'required|alpha_num|between:4,8'
+	);
+
 	/**
 	 * The database table used by the model.
 	 *
