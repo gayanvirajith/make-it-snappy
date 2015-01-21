b4ca00c642c3f88ba410396aaaf7022489f4f7f8
Gayan Virajith <gayanvirajith@gmail.com>
2015-01-21 10:46:36 +0530

    Local configurations and way/generators

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -26,7 +26,7 @@ $app = new Illuminate\Foundation\Application;
 
 $env = $app->detectEnvironment(array(
 
-	'local' => array('homestead'),
+	'local' => array('homestead', 'gayan-HP-ENVY-15-Notebook-PC'),
 
 ));
 
